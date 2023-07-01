c10cc8afad2e628b9a6f62df7d912c19510850ef
TiagoPaulin <thiago.vmp15@gmail.com>
2023-07-01 20:21:15 -0300

    comando help e outros ajustes

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import discord
 from key import token #importa do meu diretorio o arquivo que contem o token do meu bot
-# from key import cat
 import requests
-import random
-import json
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,36 +12,32 @@ TOKEN = token.get('TOKEN') #aloca o token do meu bot na constante TOKEN
 async def on_ready():
     print(f'{client.user} ficou online') #mensagem de quando o bot ficar online
 
-
-
-
-
-
-
-
 @client.event
 async def on_message(message):
 
     if message.author == client.user: #verificação para o bot responder somente mensagens de usuários
         return
 
-#comando para mandar uma imagem de cachorro aleatoria no chat (Via requisição web)
+# comando para mandar uma imagem de cachorro aleatoria no chat (Via requisição web)
     if message.content.lower() == "!dog":
-        response = requests.get("https://dog.ceo/api/breeds/image/random")
-        image = response.json()['message']
-        await message.channel.send(image)
+        response = requests.get("https://dog.ceo/api/breeds/image/random") # Faz uma requisição GET para a API Dog CEO para obter uma imagem aleatória de cachorro
+        image = response.json()['message'] # Converte a resposta da requisição em um objeto JSON e obtém o valor do campo 'message', que contém a URL da imagem
+        await message.channel.send(image) # Envia a URL da imagem do cachorro no canal de mensagem do Discord
 
-#comando para mandar uma imagem de gatinho aleatoria no chat (Via requisição web)
+# comando para mandar uma imagem de gatinho aleatoria no chat (Via requisição web)
     if message.content.lower() == "!cat":
         response = requests.get("https://api.thecatapi.com/v1/images/search")
-        # Faz uma requisição GET para a API TheCatAPI para obter uma imagem de gato
         json_data = response.json() # Converte a resposta da requisição em um objeto JSON
         if json_data: # Verifica se a resposta contém dados
             cat = json_data[0] # Acessa o primeiro objeto da lista de dados
             url = cat.get('url') # Obtém o valor do campo 'url' do objeto 'cat'
             await message.channel.send(url) # Envia a URL da imagem do gato no canal de mensagem do Discord
         else:
-            await message.channel.send("Não foi possível obter a URL da imagem do gato.") # Envia uma mensagem informando que não foi possível obter a URL da imagem do gato
+            await message.channel.send("Não encontrei nenhum gatinho </3)") # Envia uma mensagem informando que não foi possível obter a URL da imagem do gato
+
+# menu de comandos 
+    if message.content.lower() == "!help":
+        await message.channel.send(f"Olá, {message.author.name} estes são meus comandos\n1- !cat para você ver um gatinho\n2- !dog para você ver um cachorrinho")
 
 client.run(TOKEN) # inicia o Bot
 
