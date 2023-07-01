3d8f233a81723dd0e6212ec15bead88dba1d6d68
TiagoPaulin <thiago.vmp15@gmail.com>
2023-07-01 19:50:12 -0300

    gatinhos requisicao web

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 from key import token #importa do meu diretorio o arquivo que contem o token do meu bot
-# import requests
+# from key import cat
+import requests
 import random
+import json
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,6 +15,13 @@ TOKEN = token.get('TOKEN') #aloca o token do meu bot na constante TOKEN
 async def on_ready():
     print(f'{client.user} ficou online') #mensagem de quando o bot ficar online
 
+
+
+
+
+
+
+
 @client.event
 async def on_message(message):
 
@@ -22,6 +31,7 @@ async def on_message(message):
     # if message.content.lower() == "!gatinho": 
     #     await message.channel.send(f'Oi {message.author}, ainda sem gatinhos')
 
+# comando para mandar uma imagem de gatinho aleatoria no chat (somente imagens salvas em "cats")
     if message.content.lower() == "!cat":
         number = random.randint(1,10)
         file_path = f'cats/gato_icon{number}.jpg'
@@ -29,5 +39,21 @@ async def on_message(message):
         with open(file_path, 'rb') as file:
             await message.channel.send(file=discord.File(file))
 
+#comando para mandar uma imagem de cachorro aleatoria no chat (Faz uso de uma API)
+    if message.content.lower() == "!dog":
+        response = requests.get("https://dog.ceo/api/breeds/image/random")
+        image = response.json()['message']
+        await message.channel.send(image)
+
+    if message.content.lower() == "!teste":
+        response = requests.get("https://api.thecatapi.com/v1/images/search")
+        json_data = response.json()
+        
+        if json_data:  # Verifica se a resposta contém dados
+            cat = json_data[0]
+            url = cat.get('url')
+            await message.channel.send(url)
+        else:
+            await message.channel.send("Não foi possível obter a URL da imagem do gato.")
 client.run(TOKEN) # inicia o Bot
 
