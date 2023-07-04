09613fe181b9cdc94e3d77ef2b6e3efa692f7423
TiagoPaulin <thiago.vmp15@gmail.com>
2023-07-03 23:31:55 -0300

    Slash command

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import discord
 from key import token #importa do meu diretorio o arquivo que contem o token do meu bot
 import requests
+from discord import app_commands
+from discord.ext import commands 
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+# bot = commands.Bot(command_prefix='/', intents=intents)
 
 TOKEN = token.get('TOKEN') #aloca o token do meu bot na constante TOKEN
 
 @client.event
 async def on_ready():
     print(f'{client.user} ficou online') #mensagem de quando o bot ficar online
+
+# @client.tree.command(name="hello")
+# async def hello(interaction: discord.Interaction):
+#     await interaction.response.send_message(f"Hey {interaction.user.mention}! This is a slash command!", ephemeral=True)
+
 
 @client.event
 async def on_message(message):
